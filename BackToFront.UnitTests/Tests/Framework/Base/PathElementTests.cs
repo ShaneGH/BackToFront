72361ff9a2d19ec8ef0691e26a22005505f3a90a
Shane Connon <shane.connon@gmail.com>
2013-02-20 02:00:55 +0000

    Test unit test fail

--- BackToFront.UnitTests/Tests/Framework/Base/PathElementTests.cs
+++ BackToFront.UnitTests/Tests/Framework/Base/PathElementTests.cs
@@ -58,6 +58,7 @@ namespace BackToFront.UnitTests.Tests.Framework.Base
         [Test]
         public void ValidateTest_violation()
         {
+            Assert.Fail();
             // arrange
             var v = new SimpleViolation("violation");
 
