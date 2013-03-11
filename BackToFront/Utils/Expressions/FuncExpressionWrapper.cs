618965db8a25cfe07069a6bd7f816132de4d4ce3
Shane Connon <shane.connon@gmail.com>
2013-03-11 20:05:45 +0000

    Added multiple linq expression types

--- BackToFront/Utils/Expressions/FuncExpressionWrapper.cs
+++ BackToFront/Utils/Expressions/FuncExpressionWrapper.cs
@@ -4,12 +4,14 @@ using System.Linq.Expressions;
 
 namespace BackToFront.Utils.Expressions
 {
-    //TODO: more parameters
-    internal class FuncExpressionWrapper<TParam1, TOutput> : ExpressionWrapperBase<Expression<Func<TParam1, TOutput>>>
+    /// <summary>
+    /// Private class, do not export as it exposes un wanted behavior (e.g. expressions without return types)
+    /// </summary>
+    abstract class FuncExpressionWrapperBase : ExpressionWrapperBase<LambdaExpression>
     {
         public readonly ExpressionWrapperBase Body;
 
-        public FuncExpressionWrapper(Expression<Func<TParam1, TOutput>> expression)
+        protected FuncExpressionWrapperBase(LambdaExpression expression)
             : base(expression)
         {
             Body = CreateChildWrapper(expression.Body);
@@ -17,8 +19,8 @@ namespace BackToFront.Utils.Expressions
 
         public override bool IsSameExpression(ExpressionWrapperBase expression)
         {
-            if (expression is FuncExpressionWrapper<TParam1, TOutput>)
-                expression = (expression as FuncExpressionWrapper<TParam1, TOutput>).Body;
+            if (expression is FuncExpressionWrapperBase)
+                expression = (expression as FuncExpressionWrapperBase).Body;
             
             if (expression == null)
                 return false;
@@ -39,38 +41,75 @@ namespace BackToFront.Utils.Expressions
         }
     }
 
-    // TODO: refactor into 1 class and unit test better
-    internal class FuncExpressionWrapper<TParam1, TParam2, TOutput> : ExpressionWrapperBase<Expression<Func<TParam1, TParam2, TOutput>>>
+    internal class FuncExpressionWrapper<TParam1, TOutput> : FuncExpressionWrapperBase
     {
-        public readonly ExpressionWrapperBase Body;
+        public FuncExpressionWrapper(Expression<Func<TParam1, TOutput>> expression)
+            : base(expression)
+        {
+        }
+    }
 
+    internal class FuncExpressionWrapper<TParam1, TParam2, TOutput> : FuncExpressionWrapperBase
+    {
         public FuncExpressionWrapper(Expression<Func<TParam1, TParam2, TOutput>> expression)
             : base(expression)
         {
-            Body = CreateChildWrapper(expression.Body);
         }
+    }
 
-        public override bool IsSameExpression(ExpressionWrapperBase expression)
+    internal class FuncExpressionWrapper<TParam1, TParam2, TParam3, TOutput> : FuncExpressionWrapperBase
+    {
+        public FuncExpressionWrapper(Expression<Func<TParam1, TParam3, TOutput>> expression)
+            : base(expression)
         {
-            if (expression is FuncExpressionWrapper<TParam1, TParam2, TOutput>)
-                expression = (expression as FuncExpressionWrapper<TParam1, TParam2, TOutput>).Body;
-
-            if (expression == null)
-                return false;
-
-            if (Body.WrappedExpressionParameters.Count != expression.WrappedExpressionParameters.Count)
-                return false;
-
-            for (int i = 0, ii = Body.WrappedExpressionParameters.Count; i < ii; i++)
-                if (Body.WrappedExpressionParameters[i].Type != expression.WrappedExpressionParameters[i].Type)
-                    return false;
-
-            return Body.IsSameExpression(expression);
         }
+    }
 
-        protected override object OnEvaluate(IEnumerable<object> paramaters, IEnumerable<Tuple<ExpressionWrapperBase, object>> mocks)
+    internal class FuncExpressionWrapper<TParam1, TParam2, TParam3, TParam4, TOutput> : FuncExpressionWrapperBase
+    {
+        public FuncExpressionWrapper(Expression<Func<TParam1, TParam3, TParam4, TOutput>> expression)
+            : base(expression)
         {
-            return Body.Evaluate(paramaters, mocks);
+        }
+    }
+
+    internal class FuncExpressionWrapper<TParam1, TParam2, TParam3, TParam4, TParam5, TOutput> : FuncExpressionWrapperBase
+    {
+        public FuncExpressionWrapper(Expression<Func<TParam1, TParam3, TParam4, TParam5, TOutput>> expression)
+            : base(expression)
+        {
+        }
+    }
+
+    internal class FuncExpressionWrapper<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TOutput> : FuncExpressionWrapperBase
+    {
+        public FuncExpressionWrapper(Expression<Func<TParam1, TParam3, TParam4, TParam5, TParam6, TOutput>> expression)
+            : base(expression)
+        {
+        }
+    }
+
+    internal class FuncExpressionWrapper<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TParam7, TOutput> : FuncExpressionWrapperBase
+    {
+        public FuncExpressionWrapper(Expression<Func<TParam1, TParam3, TParam4, TParam5, TParam6, TParam7, TOutput>> expression)
+            : base(expression)
+        {
+        }
+    }
+
+    internal class FuncExpressionWrapper<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TParam7, TParam8, TOutput> : FuncExpressionWrapperBase
+    {
+        public FuncExpressionWrapper(Expression<Func<TParam1, TParam3, TParam4, TParam5, TParam6, TParam7, TParam8, TOutput>> expression)
+            : base(expression)
+        {
+        }
+    }
+
+    internal class FuncExpressionWrapper<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TParam7, TParam8, TParam9, TOutput> : FuncExpressionWrapperBase
+    {
+        public FuncExpressionWrapper(Expression<Func<TParam1, TParam3, TParam4, TParam5, TParam6, TParam7, TParam8, TParam9, TOutput>> expression)
+            : base(expression)
+        {
         }
     }
 }
