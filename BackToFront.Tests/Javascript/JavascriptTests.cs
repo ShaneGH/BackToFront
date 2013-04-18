6ae7fba92c90f990472b48d0341e65ce6942cc47
Shane Connon <shane.connon@gmail.com>
2013-04-18 23:39:52 +0100

    try again

--- BackToFront.Tests/Javascript/JavascriptTests.cs
+++ BackToFront.Tests/Javascript/JavascriptTests.cs
@@ -13,14 +13,16 @@ namespace BackToFront.Tests.Javascript
     public class JavascriptTests
     {
         public const string TestResultsFile = "unitTestResults.xml";
+        public const string BTF = "BackToFront";
 
         [Test]
-        [Explicit]
         public void Run()
         {
+            var dir = System.Environment.CurrentDirectory.Substring(0, System.Environment.CurrentDirectory.IndexOf(BTF) + BTF.Length + 1);
+
             Process testRunner = new Process();
 
-            testRunner.StartInfo = new ProcessStartInfo(@"..\..\..\Tools\Chutzpah\chutzpah.console.exe", @"/path ..\..\BackToFront.Tests\Javascript\UnitTests /testMode TypeScript /junit " + TestResultsFile);
+            testRunner.StartInfo = new ProcessStartInfo(string.Format(@"{0}Tools\Chutzpah\chutzpah.console.exe", dir), string.Format(@"/path {0}BackToFront.Tests\Javascript\UnitTests /testMode TypeScript /junit {1}", dir, TestResultsFile));
             testRunner.StartInfo.UseShellExecute = false;
             testRunner.StartInfo.RedirectStandardOutput = true;
             testRunner.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
