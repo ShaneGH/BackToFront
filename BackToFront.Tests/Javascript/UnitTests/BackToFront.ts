fe6ae38654f284537bfdb4727856817f1990e401
Shane Connon <shane.connon@gmail.com>
2013-04-20 14:13:55 +0100

    fix build

--- BackToFront.Tests/Javascript/UnitTests/BackToFront.ts
+++ BackToFront.Tests/Javascript/UnitTests/BackToFront.ts
@@ -2,5 +2,5 @@
 /// <reference path="../../../BackToFront.Web/Scripts/BackToFront-0.0.ts" />
 
 test("Hello", () => {
-    deepEqual(false, new __BTF.TestClass().Test());
+    deepEqual(true, new __BTF.TestClass().Test());
 });
