b8f30f53cca401a1fc890fa53f7baf85000f0d17
Shane Connon <shane.connon@gmail.com>
2013-04-21 22:18:02 +0100

    nothing of interest

---
+++ BackToFront/Enum/PathElementType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackToFront.Enum
+{
+    public enum PathElementType
+    {
+        //TODO: Remove
+        Invalid,
+
+        Rule
+    }
+}
