6b53d53c1ece7693d67c80bbde4d8c539aed3143
Shane Connon <shane.connon@gmail.com>
2013-04-21 22:22:01 +0100

    try to run js unit tests from bin/debug

--- BackToFront.Tests/Javascript/Runtime/RunTests.bat
+++ BackToFront.Tests/Javascript/Runtime/RunTests.bat
@@ -1,2 +1,2 @@
-Javascript\Chutzpah\chutzpah.console.exe /path ..\..\Javascript\UnitTests /testMode TypeScript /junit Javascript\Runtime\TestsResults.xml
+Javascript\Chutzpah\chutzpah.console.exe /path ..\..\Javascript\UnitTests /testMode TypeScript /junit bin\debug\TestsResults.xml
 EXIT 0
