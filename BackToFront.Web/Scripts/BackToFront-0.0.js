1b4d6f9546259d0d1835e61ac23d56c9f2c2656d
Shane Connon <shane.connon@gmail.com>
2013-05-11 16:18:46 +0100

    Added compile to most js expressions

--- BackToFront.Web/Scripts/BackToFront-0.0.js
+++ BackToFront.Web/Scripts/BackToFront-0.0.js
@@ -34,14 +34,29 @@ var __BTF;
                 if(item == null) {
                     throw "Item must have a value";
                 }
-                for(var i = 0, ii = properties.length; i < ii; i++) {
-                    if(item[properties[i]] == null) {
-                        throw properties[i] + " cannot be null";
-                    }
+                var failure = linq(properties).First(function (a) {
+                    return item[a] == null;
+                });
+                if(failure == null) {
+                    throw failure + " cannot be null";
                 }
             };
             Expression.prototype.Compile = function () {
+                if(!this._Compiled) {
+                    var compiled = this._Compile();
+                    this._Compiled = function (item, context) {
+                        if(!context.Break()) {
+                            compiled(item, context);
+                        }
+                    };
+                }
+                return this._Compiled;
+            };
+            Expression.prototype._Compile = function () {
                 throw "Invalid operation";
+            };
+            Expression.prototype.GetAffectedProperties = function () {
+                return [];
             };
             Expression.CreateExpression = function CreateExpression(meta) {
                 switch(meta.ExpressionType) {
@@ -74,9 +89,21 @@ var __BTF;
             function BinaryExpression(meta) {
                         _super.call(this, meta);
                 this.Required(meta, "Left", "Right");
+                if(!BinaryExpression.OperatorDictionary[this.NodeType]) {
+                    throw "Invalid Operator";
+                }
                 this.Left = Expression.CreateExpression(meta.Left);
                 this.Right = Expression.CreateExpression(meta.Right);
             }
+            BinaryExpression.OperatorDictionary = [];
+            BinaryExpression.prototype._Compile = function () {
+                var _this = this;
+                var left = this.Left.Compile();
+                var right = this.Right.Compile();
+                return function (namedArguments, context) {
+                    return BinaryExpression.OperatorDictionary[_this.NodeType](left(namedArguments, context), right(namedArguments, context));
+                };
+            };
             return BinaryExpression;
         })(Expression);
         Expressions.BinaryExpression = BinaryExpression;        
@@ -89,6 +116,16 @@ var __BTF;
                     return Expression.CreateExpression(a);
                 }).Result;
             }
+            BlockExpression.prototype._Compile = function () {
+                var children = linq(this.Expressions).Select(function (a) {
+                    return a.Compile();
+                }).Result;
+                return function (namedArguments, context) {
+                    return linq(children).Each(function (a) {
+                        return a(namedArguments, context);
+                    });
+                };
+            };
             return BlockExpression;
         })(Expression);
         Expressions.BlockExpression = BlockExpression;        
@@ -101,6 +138,14 @@ var __BTF;
                 this.IfFalse = Expression.CreateExpression(meta.IfFalse);
                 this.Test = Expression.CreateExpression(meta.Test);
             }
+            ConditionalExpression.prototype._Compile = function () {
+                var test = this.Test.Compile();
+                var ifTrue = this.IfTrue.Compile();
+                var ifFalse = this.IfFalse.Compile();
+                return function (namedArguments, context) {
+                    return test(namedArguments, context) ? ifTrue(namedArguments, context) : ifFalse(namedArguments, context);
+                };
+            };
             return ConditionalExpression;
         })(Expression);
         Expressions.ConditionalExpression = ConditionalExpression;        
@@ -109,6 +154,11 @@ var __BTF;
             function ConstantExpression(meta) {
                         _super.call(this, meta);
             }
+            ConstantExpression.prototype._Compile = function () {
+                return function (namedArguments, context) {
+                    return null;
+                };
+            };
             return ConstantExpression;
         })(Expression);
         Expressions.ConstantExpression = ConstantExpression;        
@@ -117,6 +167,11 @@ var __BTF;
             function DefaultExpression(meta) {
                         _super.call(this, meta);
             }
+            DefaultExpression.prototype._Compile = function () {
+                return function (namedArguments, context) {
+                    return null;
+                };
+            };
             return DefaultExpression;
         })(Expression);
         Expressions.DefaultExpression = DefaultExpression;        
@@ -128,6 +183,35 @@ var __BTF;
                 this.Expression = Expression.CreateExpression(meta.Expression);
                 this.MemberName = meta.MemberName;
             }
+            MemberExpression.RegexValues = (function () {
+                var index = "\\[[0-9]+\\]";
+                var indexedProperty = "[_a-zA-Z][_a-zA-Z0-9]*(" + index + ")?";
+                return {
+                    IndexedProperty: new RegExp(index + "$"),
+                    Property: new RegExp("^" + indexedProperty + "$")
+                };
+            })();
+            MemberExpression.prototype._Compile = function () {
+                var _this = this;
+                var expression = this.Expression.Compile();
+                return function (namedArguments, context) {
+                    if(!MemberExpression.RegexValues.Property.test(_this.MemberName)) {
+                        throw "Invalid property name: " + _this.MemberName;
+                    }
+                    var base = expression(namedArguments, context);
+                    if(MemberExpression.RegexValues.IndexedProperty.test(_this.MemberName)) {
+                        var property = _this.MemberName.substr(0, _this.MemberName.indexOf("[") - 1);
+                        var index = MemberExpression.RegexValues.Property.exec(_this.MemberName)[0];
+                        index = parseInt(index.substring(1, index.length - 1));
+                        base = base[property];
+                        if(base == null) {
+                            return null;
+                        }
+                        return base[index];
+                    }
+                    return base[_this.MemberName];
+                };
+            };
             return MemberExpression;
         })(Expression);
         Expressions.MemberExpression = MemberExpression;        
@@ -143,6 +227,9 @@ var __BTF;
                 this.MethodName = meta.MethodName;
                 this.MethodFullName = meta.MethodFullName;
             }
+            MethodCallExpression.prototype._Compile = function () {
+                throw "Not implemented";
+            };
             return MethodCallExpression;
         })(Expression);
         Expressions.MethodCallExpression = MethodCallExpression;        
@@ -153,6 +240,12 @@ var __BTF;
                 this.Required(meta, "Name");
                 this.Name = meta.Name;
             }
+            ParameterExpression.prototype._Compile = function () {
+                var _this = this;
+                return function (namedArguments, context) {
+                    return namedArguments[_this.Name];
+                };
+            };
             return ParameterExpression;
         })(Expression);
         Expressions.ParameterExpression = ParameterExpression;        
@@ -160,17 +253,39 @@ var __BTF;
             __extends(UnaryExpression, _super);
             function UnaryExpression(meta) {
                         _super.call(this, meta);
-                this.Required(meta, "Expression", "MemberName", "Test");
+                this.Required(meta, "Operand");
                 this.Operand = Expression.CreateExpression(meta.Operand);
             }
+            UnaryExpression.OperatorDictionary = [];
+            UnaryExpression.prototype._Compile = function () {
+                var _this = this;
+                var operand = this.Operand.Compile();
+                return function (namedArguments, context) {
+                    return UnaryExpression.OperatorDictionary[_this.NodeType](operand(namedArguments, context));
+                };
+            };
             return UnaryExpression;
         })(Expression);
         Expressions.UnaryExpression = UnaryExpression;        
-        var ExpressionInvoker = (function () {
-            function ExpressionInvoker() { }
-            return ExpressionInvoker;
-        })();
-        Expressions.ExpressionInvoker = ExpressionInvoker;        
     })(__BTF.Expressions || (__BTF.Expressions = {}));
     var Expressions = __BTF.Expressions;
+    (function (Validation) {
+        var ExpressionInvoker = (function () {
+            function ExpressionInvoker(Logic, AffectedProperties) {
+                this.Logic = Logic;
+                this.AffectedProperties = AffectedProperties;
+            }
+            return ExpressionInvoker;
+        })();
+        Validation.ExpressionInvoker = ExpressionInvoker;        
+        var ValidationContext = (function () {
+            function ValidationContext() { }
+            ValidationContext.prototype.Break = function () {
+                return false;
+            };
+            return ValidationContext;
+        })();
+        Validation.ValidationContext = ValidationContext;        
+    })(__BTF.Validation || (__BTF.Validation = {}));
+    var Validation = __BTF.Validation;
 })(__BTF || (__BTF = {}));
--- BackToFront.Web/Scripts/BackToFront-0.0.ts
+++ BackToFront.Web/Scripts/BackToFront-0.0.ts
@@ -33,17 +33,31 @@ module __BTF {
                     throw "Item must have a value";
                 }
 
-                for (var i = 0, ii = properties.length; i < ii; i++) {
-                    if (item[properties[i]] == null) {
-                        throw properties[i] + " cannot be null";
-                    }
+                var failure = linq(properties).First(a => item[a] == null);
+                if (failure == null) {
+                    throw failure + " cannot be null";
                 }
             }
 
-            Compile() : ExpressionInvoker {
+            private _Compiled: Validation.ExpressionInvokerAction;
+            Compile(): Validation.ExpressionInvokerAction {
+                if (!this._Compiled) {
+                    var compiled = this._Compile();
+                    this._Compiled = (item, context) => {
+                        if (!context.Break())
+                            compiled(item, context);
+                    };
+                }
+
+                return this._Compiled;
+            }
+
+            _Compile(): Validation.ExpressionInvokerAction {
                 // child classes must implement this
                 throw "Invalid operation";
             }
+
+            GetAffectedProperties(): string[] { return []; }
 
             static CreateExpression(meta: M.ExpressionMeta): Expression {
                 switch (meta.ExpressionType) {
@@ -75,13 +89,23 @@ module __BTF {
             Left: Expression;
             Right: Expression;
 
+            private static OperatorDictionary: { (left, right): any; }[] = [];
+
             constructor(meta: M.BinaryExpressionMeta) {
                 super(meta);
 
                 this.Required(meta, "Left", "Right");
+                if (!BinaryExpression.OperatorDictionary[this.NodeType])
+                    throw "Invalid Operator";
 
                 this.Left = Expression.CreateExpression(meta.Left);
                 this.Right = Expression.CreateExpression(meta.Right);
+            }
+
+            _Compile(): Validation.ExpressionInvokerAction {
+                var left = this.Left.Compile();
+                var right = this.Right.Compile();
+                return (namedArguments, context) => BinaryExpression.OperatorDictionary[this.NodeType](left(namedArguments, context), right(namedArguments, context))
             }
         }
 
@@ -93,6 +117,11 @@ module __BTF {
 
                 this.Required(meta, "Expressions");
                 this.Expressions = linq(meta.Expressions).Select(a => Expression.CreateExpression(a)).Result;
+            }
+
+            _Compile(): Validation.ExpressionInvokerAction {
+                var children = linq(this.Expressions).Select(a => a.Compile()).Result;
+                return (namedArguments, context) => linq(children).Each(a => a(namedArguments, context));
             }
         }
 
@@ -109,11 +138,24 @@ module __BTF {
                 this.IfFalse = Expression.CreateExpression(meta.IfFalse);
                 this.Test = Expression.CreateExpression(meta.Test);
             }
+
+            _Compile(): Validation.ExpressionInvokerAction {
+                var test = this.Test.Compile();
+                var ifTrue = this.IfTrue.Compile();
+                var ifFalse = this.IfFalse.Compile();
+
+                return (namedArguments, context) => test(namedArguments, context) ? ifTrue(namedArguments, context) : ifFalse(namedArguments, context);
+            }
         }
 
         export class ConstantExpression extends Expression {
             constructor(meta: M.ConstantExpressionMeta) {
                 super(meta);
+            }
+
+            //TODO
+            _Compile(): Validation.ExpressionInvokerAction {
+                return (namedArguments, context) => null;
             }
         }
 
@@ -121,18 +163,64 @@ module __BTF {
             constructor(meta: M.ExpressionMeta) {
                 super(meta);
             }
+
+            //TODO
+            _Compile(): Validation.ExpressionInvokerAction {
+                return (namedArguments, context) => null;
+            }
+        }
+
+        export interface MemberExpressionRegex {
+            Property: RegExp;
+            IndexedProperty: RegExp;
         }
 
         export class MemberExpression extends Expression {
+            static RegexValues: MemberExpressionRegex = (() => {
+                var index = "\\[[0-9]+\\]";
+                var indexedProperty = "[_a-zA-Z][_a-zA-Z0-9]*(" + index + ")?";
+
+                return {
+                    IndexedProperty: new RegExp(index + "$"),
+                    Property: new RegExp("^" + indexedProperty + "$")
+                };
+            })();
+
             Expression: Expression;
             MemberName: string;
-
+            
             constructor(meta: M.MemberExpressionMeta) {
                 super(meta);
 
                 this.Required(meta, "Expression", "MemberName", "Test");
                 this.Expression = Expression.CreateExpression(meta.Expression);
                 this.MemberName = meta.MemberName;
+            }
+
+            // TODO: taking pessimistic view of properties with regards to arrays
+            // TODO: not throwing null exceptions
+            // TODO: not validating at compile time, but at run time
+            _Compile(): Validation.ExpressionInvokerAction {
+                var expression = this.Expression.Compile();
+                
+                return (namedArguments, context) => {
+                    if (!MemberExpression.RegexValues.Property.test(this.MemberName)) {
+                        throw "Invalid property name: " + this.MemberName;
+                    }
+
+                    var base = expression(namedArguments, context);
+                    if (MemberExpression.RegexValues.IndexedProperty.test(this.MemberName)) {
+                        var property = this.MemberName.substr(0, this.MemberName.indexOf("[") - 1);
+                        var index: any = MemberExpression.RegexValues.Property.exec(this.MemberName)[0];
+                        index = parseInt(index.substring(1, index.length - 1));
+
+                        base = base[property];
+                        if (base == null) return null;
+                        return base[index];
+                    }
+
+                    return base[this.MemberName];
+                };
             }
         }
 
@@ -151,6 +239,12 @@ module __BTF {
                 this.MethodName = meta.MethodName;
                 this.MethodFullName = meta.MethodFullName;
             }
+
+            // TODO
+            _Compile(): Validation.ExpressionInvokerAction {
+                throw "Not implemented";
+            }
+
         }
 
         export class ParameterExpression extends Expression {
@@ -162,20 +256,45 @@ module __BTF {
                 this.Required(meta, "Name");
                 this.Name = meta.Name;
             }
+
+            _Compile(): Validation.ExpressionInvokerAction {
+                return (namedArguments, context) => namedArguments[this.Name];
+            }
         }
 
         export class UnaryExpression extends Expression {
+
+            private static OperatorDictionary: { (operand): any; }[] = [];
+
             Operand: Expression;
 
             constructor(meta: M.UnaryExpressionMeta) {
                 super(meta);
 
-                this.Required(meta, "Expression", "MemberName", "Test");
+                this.Required(meta, "Operand");
                 this.Operand = Expression.CreateExpression(meta.Operand);
             }
+
+            _Compile(): Validation.ExpressionInvokerAction {
+                var operand = this.Operand.Compile();
+                return (namedArguments, context) => UnaryExpression.OperatorDictionary[this.NodeType](operand(namedArguments, context))
+            }
+        }
+    }
+
+    export module Validation {
+
+        export interface ExpressionInvokerAction {
+            (namedArguments: any, context: ValidationContext): any;
         }
 
         export class ExpressionInvoker {
+            constructor(public Logic: ExpressionInvokerAction, public AffectedProperties: string[]) { }
+        }
+
+        export class ValidationContext {
+            // TODO
+            Break(): bool { return false; }
         }
     }
 }
