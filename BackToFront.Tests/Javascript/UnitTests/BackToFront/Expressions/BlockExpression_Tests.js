bb35efa9eba90d9b7f7596f8601a3664a8b42614
Shane Connon <shane.connon@gmail.com>
2013-05-23 23:32:04 +0100

    More unit tests

---
+++ BackToFront.Tests/Javascript/UnitTests/BackToFront/Expressions/BlockExpression_Tests.js
@@ -0,0 +1,104 @@
+﻿
+// Chutzpah
+/// <reference path="../../../../Scripts/build/BackToFront.debug.js" />
+/// <reference path="../../../Base/testUtils.js" />
+
+test("Warmup", function () { expect(0); });
+
+(function (moduleName) {
+
+    var createExpression = __BTF.Expressions.Expression.CreateExpression;
+    var require = __BTF.Sanitizer.Require;
+
+    module(moduleName, {
+        setup: function () {
+            __BTF.Expressions.Expression.CreateExpression = createExpression;
+            __BTF.Sanitizer.Require = require;
+        },
+        teardown: function () {
+            __BTF.Expressions.Expression.CreateExpression = createExpression;
+            __BTF.Sanitizer.Require = require;
+        }
+    });
+
+    // Constructor test OK
+    (function (testName) {
+        test(testName, function () {
+            var ex = new tUtil.Expect("require", "ex1", "ex2");
+
+            // arrange
+            var meta = { Expressions: ["ex1", "ex2"], NodeType: __BTF.Meta.ExpressionType.Add };
+
+            // first Sanitizer is in parent class
+            var skip = true;
+            __BTF.Sanitizer.Require = function (input1, input2) {
+                if (skip) {
+                    skip = false;
+                    return;
+                }
+
+                ex.ExpectationReached.push("require");
+
+                assert.strictEqual(input1, meta);
+
+                assert.deepEqual(input2.inputName, "Expressions");
+                assert.deepEqual(input2.inputConstructor, Array);
+            };
+
+            __BTF.Expressions.Expression.CreateExpression = function (input) {
+                ex.ExpectationReached.push(input);
+                return input + "XX";
+            };
+
+            // act
+            var actual = new __BTF.Expressions.BlockExpression(meta);
+
+            // assert
+            ex.VerifyOrderedExpectations();
+            assert.deepEqual(2, actual.Expressions.length);
+            assert.deepEqual("ex1XX", actual.Expressions[0]);
+            assert.deepEqual("ex2XX", actual.Expressions[1]);
+        });
+    })("Constructor test OK");
+
+    // _Compile test
+    (function (testName) {
+        test(testName, function () {
+            var ex = new tUtil.Expect("ex1", "ex2");
+
+            var namedArguments = {};
+            var context = {};
+
+            // arrange
+            var _this = {
+                Expressions: [
+                {
+                    Compile: function () {
+                        return function (na, ctxt) {
+                            ex.ExpectationReached.push("ex1");
+                            assert.strictEqual(na, namedArguments);
+                            assert.strictEqual(ctxt, context);
+                            return "ex1";
+                        }
+                    }
+                }, {
+                    Compile: function () {
+                        return function (na, ctxt) {
+                            ex.ExpectationReached.push("ex2");
+                            assert.strictEqual(na, namedArguments);
+                            assert.strictEqual(ctxt, context);
+                            return "ex2";
+                        }
+                    }
+                }]
+            };
+
+            // act
+            var result = __BTF.Expressions.BlockExpression.prototype._Compile.call(_this);
+
+            // assert
+            result(namedArguments, context);
+            ex.VerifyOrderedExpectations();
+        });
+    })("_Compile test");
+})("__BTF.Expressions.BlockExpression");
---
+++ BackToFront.Tests/Javascript/UnitTests/BackToFront/Expressions/ConditionalExpression_Tests.js
@@ -0,0 +1,130 @@
+﻿
+// Chutzpah
+/// <reference path="../../../../Scripts/build/BackToFront.debug.js" />
+/// <reference path="../../../Base/testUtils.js" />
+
+test("Warmup", function () { expect(0); });
+
+(function (moduleName) {
+
+    var createExpression = __BTF.Expressions.Expression.CreateExpression;
+    var require = __BTF.Sanitizer.Require;
+
+    module(moduleName, {
+        setup: function () {
+            __BTF.Expressions.Expression.CreateExpression = createExpression;
+            __BTF.Sanitizer.Require = require;
+        },
+        teardown: function () {
+            __BTF.Expressions.Expression.CreateExpression = createExpression;
+            __BTF.Sanitizer.Require = require;
+        }
+    });
+
+    // Constructor test OK
+    (function (testName) {
+        test(testName, function () {
+            var ex = new tUtil.Expect("require", "true", "false", "test");
+            debugger;
+            // arrange
+            var meta = { IfTrue: "true", IfFalse: "false", Test: "test", NodeType: __BTF.Meta.ExpressionType.Add };
+
+            // first Sanitizer is in parent class
+            var skip = true;
+            __BTF.Sanitizer.Require = function (input1, input2, input3, input4) {
+                if (skip) {
+                    skip = false;
+                    return;
+                }
+
+                ex.ExpectationReached.push("require");
+
+                assert.strictEqual(input1, meta);
+
+                assert.deepEqual(input2.inputName, "IfTrue");
+                assert.deepEqual(input2.inputType, "object");
+
+                assert.deepEqual(input3.inputName, "IfFalse");
+                assert.deepEqual(input3.inputType, "object");
+
+                assert.deepEqual(input4.inputName, "Test");
+                assert.deepEqual(input4.inputType, "object");
+            };
+
+            __BTF.Expressions.Expression.CreateExpression = function (input) {
+                ex.ExpectationReached.push(input);
+                return input + "XX";
+            };
+
+            // act
+            var actual = new __BTF.Expressions.ConditionalExpression(meta);
+
+            // assert
+            ex.VerifyOrderedExpectations();
+            assert.deepEqual("trueXX", actual.IfTrue);
+            assert.deepEqual("falseXX", actual.IfFalse);
+            assert.deepEqual("testXX", actual.Test);
+        });
+    })("Constructor test OK");
+
+    // _Compile test
+    var compileTest = function (test) {
+        var ex = new tUtil.Expect("test", "true", "false");
+
+        var namedArguments = {};
+        var context = {};
+
+        // arrange
+        var _this = {
+            IfTrue: {
+                Compile: function () {
+                    ex.ExpectationReached.push("true");
+                    return function (na, ctxt) {
+                        assert.strictEqual(na, namedArguments);
+                        assert.strictEqual(ctxt, context);
+                        return true;
+                    }
+                }
+            },
+            IfFalse: {
+                Compile: function () {
+                    ex.ExpectationReached.push("false");
+                    return function (na, ctxt) {
+                        assert.strictEqual(na, namedArguments);
+                        assert.strictEqual(ctxt, context);
+                        return false;
+                    }
+                }
+            },
+            Test: {
+                Compile: function () {
+                    ex.ExpectationReached.push("test");
+                    return function (na, ctxt) {
+                        assert.strictEqual(na, namedArguments);
+                        assert.strictEqual(ctxt, context);
+                        return test;
+                    }
+                }
+            }
+        };
+
+        // act
+        var result = __BTF.Expressions.ConditionalExpression.prototype._Compile.call(_this);
+
+        // assert
+        assert.deepEqual(result(namedArguments, context), test);
+        ex.VerifyOrderedExpectations();
+    }
+
+    (function (testName) {
+        test(testName, function () {
+            compileTest(true);
+        });
+    })("_Compile test: true");
+
+    (function (testName) {
+        test(testName, function () {
+            compileTest(false);
+        });
+    })("_Compile test: false");
+})("__BTF.Expressions.ConditionalExpression");
