33be59d4f46eb4957f1939e60b50920e90ae9889
Shane Connon <shane.connon@gmail.com>
2013-06-10 23:53:24 +0100

    bracketed binary expression

--- WebExpressions/Scripts/WebExpressions/BinaryExpression.ts
+++ WebExpressions/Scripts/WebExpressions/BinaryExpression.ts
@@ -74,7 +74,7 @@ module WebExpressions {
                 var left = this.Left.EvalExpression();
                 var right = this.Right.EvalExpression();
                 return {
-                    Expression: left.Expression + BinaryExpression.OperatorStringDictionary[this.NodeType] + right.Expression,
+                    Expression: "(" + left.Expression + BinaryExpression.OperatorStringDictionary[this.NodeType] + right.Expression + ")",
                     Constants: left.Constants.Merge(right.Constants)
                 };
             }                 
